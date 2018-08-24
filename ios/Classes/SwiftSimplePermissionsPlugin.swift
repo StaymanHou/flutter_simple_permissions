1e7afd5c03c3c3f334ad9715d50cf569a92690d8
Stayman Hou <stayman.hou@gmail.com>
2018-08-24 15:50:08 -0400

    remove 9.0 features

--- ios/Classes/SwiftSimplePermissionsPlugin.swift
+++ ios/Classes/SwiftSimplePermissionsPlugin.swift
@@ -8,18 +8,18 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
     var whenInUse = false
     var result: FlutterResult? = nil
     var locationManager = CLLocationManager()
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "simple_permissions", binaryMessenger: registrar.messenger())
         let instance = SwiftSimplePermissionsPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         locationManager.delegate = self
         let method = call.method
         let dic = call.arguments as? [String: Any]
-        
+
         switch(method) {
         case "checkPermission":
             if let permission = dic?["permission"] as? String {
@@ -27,24 +27,24 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             } else {
                 result(FlutterError(code: "permission missing", message: nil, details: nil))
             }
-            
+
         case "getPermissionStatus":
             if let permission = dic?["permission"] as? String {
                 getPermissionStatus(permission, result: result)
             } else {
                 result(FlutterError(code: "permission missing", message: nil, details: nil))
             }
-            
+
         case "requestPermission":
             if let permission = dic?["permission"] as? String {
                 requestPermission(permission, result: result)
             } else {
                 result(FlutterError(code: "permission missing", message: nil, details: nil))
             }
-            
+
         case "getPlatformVersion":
             result("iOS " + UIDevice.current.systemVersion)
-            
+
         case "openSettings":
             if let url = URL(string: UIApplicationOpenSettingsURLString) {
                 if UIApplication.shared.canOpenURL(url) {
@@ -57,76 +57,76 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
                     }
                 }
             }
-            
+
         default:
             result(FlutterMethodNotImplemented)
-            
+
         }
-        
+
     }
-    
+
     // Request permission
     private func requestPermission(_ permission: String, result: @escaping FlutterResult) {
         switch(permission) {
         case "RECORD_AUDIO":
             requestAudioPermission(result: result)
-            
+
         case "CAMERA":
             requestCameraPermission(result: result)
-            
+
         case "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION", "WHEN_IN_USE_LOCATION":
             self.result = result
             requestLocationWhenInUsePermission()
-            
+
         case "ALWAYS_LOCATION":
             self.result = result
             requestLocationAlwaysPermission()
-            
+
         case "READ_CONTACTS", "WRITE_CONTACTS":
             requestContactPermission(result: result)
-            
+
         default:
             result(FlutterMethodNotImplemented)
-            
+
         }
     }
-    
+
     // Check permissions
     private func checkPermission(_ permission: String, result: @escaping FlutterResult) {
         switch(permission) {
         case "RECORD_AUDIO":
             result(checkAudioPermission())
-            
+
         case "CAMERA":
             result(checkCameraPermission())
-            
+
         case "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION", "WHEN_IN_USE_LOCATION":
             result(checkLocationWhenInUsePermission())
-            
+
         case "READ_CONTACTS", "WRITE_CONTACTS":
             result(checkContactPermission())
-            
+
         case "ALWAYS_LOCATION":
             result(checkLocationAlwaysPermission())
-            
+
         default:
             result(FlutterMethodNotImplemented)
-            
+
         }
     }
-    
+
     // Get permissions status
     private func getPermissionStatus (_ permission: String, result: @escaping FlutterResult) {
         switch(permission) {
         case "RECORD_AUDIO":
             result(getAudioPermissionStatus().rawValue)
-            
+
         case "READ_CONTACTS", "WRITE_CONTACTS":
             result(getContactPermissionStatus().rawValue)
-            
+
         case "CAMERA":
             result(getCameraPermissionStatus().rawValue)
-            
+
         case "ACCESS_COARSE_LOCATION", "ACCESS_FINE_LOCATION", "WHEN_IN_USE_LOCATION":
             let status = CLLocationManager.authorizationStatus()
             if (status == .authorizedAlways || status == .authorizedWhenInUse) {
@@ -135,7 +135,7 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             else {
                 result(status.rawValue)
             }
-            
+
         case "ALWAYS_LOCATION":
             let status = CLLocationManager.authorizationStatus()
             if (status == .authorizedAlways) {
@@ -147,24 +147,24 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             else {
                 result(status.rawValue)
             }
-            
+
         default:
             result(FlutterMethodNotImplemented)
-            
+
         }
     }
-    
+
     //-----------------------------------------
     // Location
     private func checkLocationAlwaysPermission() -> Bool {
         return CLLocationManager.authorizationStatus() == .authorizedAlways
     }
-    
+
     private func checkLocationWhenInUsePermission() -> Bool {
         let authStatus = CLLocationManager.authorizationStatus()
         return  authStatus == .authorizedAlways  || authStatus == .authorizedWhenInUse
     }
-    
+
     private func requestLocationWhenInUsePermission() -> Void {
         if (CLLocationManager.authorizationStatus() == .notDetermined) {
             self.whenInUse = true
@@ -174,7 +174,7 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             self.result?(checkLocationWhenInUsePermission())
         }
     }
-    
+
     private func requestLocationAlwaysPermission() -> Void {
         if (CLLocationManager.authorizationStatus() == .notDetermined) {
         self.whenInUse = false
@@ -184,14 +184,14 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             self.result?(checkLocationAlwaysPermission())
         }
     }
-    
+
     public func locationManager(_ manager: CLLocationManager,
                          didChangeAuthorization status: CLAuthorizationStatus) {
         if (whenInUse)  {
             switch status {
             case .authorizedAlways, .authorizedWhenInUse:
                 self.result?(true)
-                
+
             default:
                 self.result?(false)
             }
@@ -200,34 +200,17 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             self.result?(status == .authorizedAlways)
         }
     }
-    
-    //-----------------------------
-    // Contact
-    
-    private func getContactPermissionStatus() -> CNAuthorizationStatus {
-       return CNContactStore.authorizationStatus(for: CNEntityType.contacts)
-    }
-    
-    private func checkContactPermission() -> Bool {
-        return getContactPermissionStatus() == .authorized
-    }
-    
-    private func requestContactPermission(result: @escaping FlutterResult) -> Void {
-        CNContactStore().requestAccess(for: CNEntityType.contacts) { (access, error) in
-            result(access)
-        }
-    }
-    
+
     //---------------------------------
     // Audio
     private func checkAudioPermission() -> Bool {
         return getAudioPermissionStatus() == .authorized
     }
-    
+
     private func getAudioPermissionStatus() -> AVAuthorizationStatus {
         return AVCaptureDevice.authorizationStatus(for: AVMediaType.audio)
     }
-    
+
     private func requestAudioPermission(result: @escaping FlutterResult) -> Void {
         if (AVAudioSession.sharedInstance().responds(to: #selector(AVAudioSession.requestRecordPermission(_:)))) {
             AVAudioSession.sharedInstance().requestRecordPermission({granted in
@@ -235,18 +218,18 @@ public class SwiftSimplePermissionsPlugin: NSObject, FlutterPlugin, CLLocationMa
             })
         }
     }
-    
+
     //-----------------------------------
     // Camera
     private func checkCameraPermission()-> Bool {
       return getCameraPermissionStatus() == .authorized
     }
-    
+
     private func getCameraPermissionStatus() -> AVAuthorizationStatus {
         return AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
     }
-    
-    
+
+
     private func requestCameraPermission(result: @escaping FlutterResult) -> Void {
         AVCaptureDevice.requestAccess(for: AVMediaType.video) { response in
             result(response)
